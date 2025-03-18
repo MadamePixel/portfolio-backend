82e772a152afb7103c2d2c336e5a41f185f17ab5
MadamePixel <amber.aspeslagh@gmail.com>
2025-03-18 13:15:44 +0100

    change plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,17 @@
 module.exports = ({ env }) => ({
-  "users-permissions": {
+  'users-permissions': {
     config: {
-      jwtSecret: env("JWT_SECRET"),
+      jwtSecret: env('JWT_SECRET'),
     },
   },
   upload: {
     config: {
-      provider: "local",
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
       actionOptions: {
         upload: {},
         delete: {},
